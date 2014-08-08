0dac1365334e13afd88bf2bc9664cd33a56698dc
Steven Ramsay <steven.ramsay@outlook.com>
2014-08-08 18:32:16 +0100

    Added canonisation.

--- IMJEquiv/Syntax.fs
+++ IMJEquiv/Syntax.fs
@@ -17,20 +17,23 @@ type Term =
   | VEq of Ident * Ident
   | Seq of Term * Term
   | Cond of Term * Term * Term
-  | Assn of Term * Term
+  | Assn of Term * FldId * Term
   | Fld of Term * FldId
   | VFld of Ident * FldId
   | Call of Term * MethId * List<Term>
   | Cast of IntId * Term
   | Let of Ident * Term * Term
+  | LetCast of Ident * IntId * Ident * Term
   | LetCl of Ident * Ident * MethId * List<Term> * Term
   | While of Term * Term
   | New of Ident * IntId * List<MethSpec>
+  | Null 
 
   member x.Show = x.ToString ()
 
   override x.ToString () : String =
     match x with
+    | Null -> "null"
     | BVar s -> s
     | Num n  -> n.ToString ()
     | Skip   -> "skip"
@@ -39,12 +42,13 @@ type Term =
     | VEq (x,y) -> sprintf "%O = %O" x y
     | Seq (x,y) -> sprintf "%O; %O" x y
     | Cond (x,y,z) -> sprintf "if %O then %O else %O" x y z
-    | Assn (x,y) -> sprintf "%O := %O" x y
+    | Assn (x,f,y) -> sprintf "%O.%O := %O" x f y
     | Fld (s,f)  -> sprintf "%O.%O" s f
     | VFld (s,f) -> sprintf "%O.%O" s f
     | Call (x,m,ms) -> sprintf "%O.%O(%s)" x m (List.toStringWithDelims "" ", " "" ms)
     | Cast (s,x) -> sprintf "(%O)%O" s x
     | Let (s,x,y) -> sprintf "let %O = %O in %O" s x y
+    | LetCast (x, i, y, m) -> sprintf "let %O = (%O)%O in %O" x i y m
     | LetCl (s,x,m,ms,b) -> sprintf "let %O = %O.%O(%s) in %O" s x m (List.toStringWithDelims "" ", " "" ms) b
     | While (x,y) -> sprintf "while %O do %O" x y
     | New (x,i,ms) -> sprintf "new { %O: %O; %s }" x i (List.toStringWithDelims "" ", " "" ms)
