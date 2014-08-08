5b5fea2863843f78f75e1d58a9748546aedaf2cd
Steven Ramsay <steven.ramsay@outlook.com>
2014-08-08 08:35:18 +0100

    Draft parser.

---
+++ IMJEquiv/Syntax.fs
@@ -0,0 +1,64 @@
+﻿namespace IMJEquiv
+
+type Ident = String
+
+type FldId = String
+type MethId = String
+type IntId = String
+
+/// The type of terms, follows the grammar of the paper.
+[<StructuredFormatDisplayAttribute("{Show}")>]
+type Term = 
+  | BVar of Ident
+  | Num of Int32
+  | Skip
+  | Plus of Term * Term
+  | Eq of Term * Term
+  | VEq of Ident * Ident
+  | Seq of Term * Term
+  | Cond of Term * Term * Term
+  | Assn of Term * Term
+  | Fld of Term * FldId
+  | VFld of Ident * FldId
+  | Call of Term * MethId * List<Term>
+  | Cast of IntId * Term
+  | Let of Ident * Term * Term
+  | LetCl of Ident * Ident * MethId * List<Term> * Term
+  | While of Term * Term
+  | New of Ident * IntId * List<MethSpec>
+
+  member x.Show = x.ToString ()
+
+  override x.ToString () : String =
+    match x with
+    | BVar s -> s
+    | Num n  -> n.ToString ()
+    | Skip   -> "skip"
+    | Plus (x,y) -> sprintf "%O + %O" x y
+    | Eq (x,y) -> sprintf "%O = %O" x y
+    | VEq (x,y) -> sprintf "%O = %O" x y
+    | Seq (x,y) -> sprintf "%O; %O" x y
+    | Cond (x,y,z) -> sprintf "if %O then %O else %O" x y z
+    | Assn (x,y) -> sprintf "%O := %O" x y
+    | Fld (s,f)  -> sprintf "%O.%O" s f
+    | VFld (s,f) -> sprintf "%O.%O" s f
+    | Call (x,m,ms) -> sprintf "%O.%O(%s)" x m (List.toStringWithDelims "" ", " "" ms)
+    | Cast (s,x) -> sprintf "(%O)%O" s x
+    | Let (s,x,y) -> sprintf "let %O = %O in %O" s x y
+    | LetCl (s,x,m,ms,b) -> sprintf "let %O = %O.%O(%s) in %O" s x m (List.toStringWithDelims "" ", " "" ms) b
+    | While (x,y) -> sprintf "while %O do %O" x y
+    | New (x,i,ms) -> sprintf "new { %O: %O; %s }" x i (List.toStringWithDelims "" ", " "" ms)
+
+
+and [<StructuredFormatDisplay("{Show}")>] MethSpec = 
+  {
+    Name: Ident
+    Vars: List<Ident> 
+    Body: Term
+  }
+
+  member x.Show = x.ToString ()
+
+  override x.ToString () : String =
+    sprintf "%s: \\%s.%O" x.Name (List.toStringWithDelims "" " " "" x.Vars) x.Body
+
---
+++ IMJEquiv/Testing.fsx
@@ -0,0 +1,35 @@
+﻿/// This script is for very quickly testing things
+/// it requires the solution to have been built before
+/// in order for the referenced DLLs to be in place.
+
+#r @"..\packages\FsLexYacc.Runtime.6.0.2\lib\net40\FsLexYacc.Runtime.dll"
+#r @"..\..\Utils\bin\Debug\Utils.dll"
+
+#load "Syntax.fs"
+#load "Parser.fs"
+#load "Lexer.fs"
+#load "Parsing.fs"
+
+open IMJEquiv
+
+let pterm t = 
+  try Parsing.term "" t with
+  | Parser.ParseError (s,l,c) -> failwithf "Parse Error %d:%d: %s." l c s
+
+pterm "3 + 4"
+pterm "x := 3 + 4"
+pterm "let x = 2 + 4 in x"
+pterm "if (let x = 2 + 4 in x) then 3 else 6"
+pterm "new { x : I; }"
+pterm "new { x : I; m : \\x y.x }"
+pterm "new { x : I; m1: \\x.x, m2: \\y.y  }"
+pterm "new { x : I; m: \\x.let z = if b then 3 else 4 in z + 4 }"
+pterm "y.assert(x.val = 2)"
+pterm """
+  let x = new { x : Varint; } in 
+  new { z: I1;
+    run: \u. if x.val = 0 then (x.val := 1; f.run(); x.assert(x.val=2)) else (if x.val=1 then x.val:=2 else div)
+  }
+"""
+pterm "while (if b then 2 else 0) do skip"
+
