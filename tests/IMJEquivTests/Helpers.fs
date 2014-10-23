d8dcc6b8b62bc28711a00fa055fe2255e68936d9
Steven Ramsay <steven.ramsay@outlook.com>
2014-10-23 17:10:49 +0100

    Added more support for testing

---
+++ tests/IMJEquivTests/Helpers.fs
@@ -0,0 +1,25 @@
+﻿namespace IMJEquiv
+
+open NUnit.Framework
+
+[<AutoOpen>]
+module Helpers =
+  
+  let ptm t = 
+    try Parsing.term "" t with
+    | Parser.ParseError (s,l,c) -> failwithf "Parse Error %d:%d: %s." l c s
+
+  let pitbl t = 
+    try Parsing.itbl "" t with
+    | Parser.ParseError (s,l,c) -> failwithf "Parse Error %d:%d: %s." l c s
+
+  let ptyenv t = 
+    try Parsing.tyenv "" t with
+    | Parser.ParseError (s,l,c) -> failwithf "Parse Error %d:%d: %s." l c s
+
+  let ptytm t =
+    try Parsing.tytm "" t with
+    | Parser.ParseError (s,l,c) -> failwithf "Parse Error %d:%d: %s." l c s
+
+  let AssertAlphaEqual (s: Term, t: Term) : Unit =
+    if not (Term.alphaEq s t) then Assert.Fail ("Expected: {0}\n But was: {1}\n", s, t)
