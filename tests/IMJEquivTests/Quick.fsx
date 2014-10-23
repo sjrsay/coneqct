e8991bf5a6c42413c729f2d204d3e22431cf9ca4
Steven Ramsay <steven.ramsay@live.com>
2014-10-23 17:10:49 +0100

    Added more support for testing

---
+++ tests/IMJEquivTests/Quick.fsx
@@ -0,0 +1,12 @@
+﻿#r @"..\..\packages\NUnit.2.6.3\lib\nunit.framework.dll"
+#r @"..\..\bin\Utils.dll"
+#r @"..\..\bin\IMJEquiv.exe"
+
+open IMJEquiv
+#load "Helpers.fs"
+open IMJEquiv
+
+let s = ptm "let x = 3 in x"
+let t = ptm "let y = 3 in y"
+Term.alphaEq s t
+
