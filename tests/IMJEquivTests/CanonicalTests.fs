6952d93c47a5b29e134c7496a52656baca826c31
Steven Ramsay <steven.ramsay@outlook.com>
2014-10-23 17:11:11 +0100

    Added simple canonical form transformation tests.

--- tests/IMJEquivTests/CanonicalTests.fs
+++ tests/IMJEquivTests/CanonicalTests.fs
@@ -2,10 +2,43 @@
 
 open NUnit.Framework
 
+(*
+  To test that canonical forms are created correctly, we create a typed term `G |- t`,
+  canonise it to obtain `c` then convert `c` back to a term `t'` and check `t = t'`
+  up to renaming of bound variables.
+*)
+
 [<Test>]
-let ``hello`` () =
+let ``variables are already canonical`` () =
   let tyEnv, tm = ptytm "x : int |- x "
   let result = Canonical.toTerm (Canonical.canonise tyEnv tm)
   let expected = ptm "x"
-  printfn "%A" result
-  Assert.AreEqual(expected,result)
+  AssertAlphaEqual(expected, result)
+
+[<Test>]
+let ``null is already canonical`` () =
+  let tyEnv, tm = ptytm "|- null"
+  let result = Canonical.toTerm (Canonical.canonise tyEnv tm)
+  let expected = ptm "null"
+  AssertAlphaEqual(expected, result)
+
+[<Test>]
+let ``ints are put in a let`` () =
+  let tyEnv, tm = ptytm "|- 3"
+  let result = Canonical.toTerm (Canonical.canonise tyEnv tm)
+  let expected = ptm "let x = 3 in x"     
+  AssertAlphaEqual(expected, result)
+
+[<Test>]
+let ``skip is put in a let`` () =
+  let tyEnv, tm = ptytm "|- skip"
+  let result = Canonical.toTerm (Canonical.canonise tyEnv tm)
+  let expected = ptm "let x = skip in x"
+  AssertAlphaEqual (expected, result)
+
+[<Test>]
+let ``complex additions require lemma 34`` () =
+  let tyEnv, tm = ptytm "|- x + (let y = 3 in y)"
+  let result = Canonical.toTerm (Canonical.canonise tyEnv tm)
+  let expected = ptm "let z1 = 3 in let z2 = x + z1 in z2"
+  AssertAlphaEqual (expected, result)
