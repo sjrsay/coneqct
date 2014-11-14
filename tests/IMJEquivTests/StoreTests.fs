674b64830997c7c64bee844948a776ad7550c706
ster <ster@localhost>
2014-11-14 13:57:06 +0000

    Modified stores tests to include null

--- tests/IMJEquivTests/StoreTests.fs
+++ tests/IMJEquivTests/StoreTests.fs
@@ -15,7 +15,7 @@ let ``generate stores with numbers`` () =
   let d = pitbl "I = { f: int }, J = { g: void }"
   let s = pstore "r1 : I = { f = 3 }"
   let z = set [(3, "I"); (4, "I"); (5, "J")]
-  let actual = Store.stores' 2 d s z
+  let actual = Store.stores' 2 d (Store.tySupp d s) z
   let expected = [
       pstore "r3 : I = { f = 0 }, r4 : I = { f = 0 }, r5 : J = { g = * }"
       pstore "r3 : I = { f = 0 }, r4 : I = { f = 1 }, r5 : J = { g = * }"
@@ -34,17 +34,24 @@ let ``generate stores with registers`` () =
   let d = pitbl "I = { f: J }, J = { g: void }"
   let s = pstore "r1 : I = { f = r4 }, r2 : I = { f = r4 }, r3 : J = { g = * }, r4 : J = { g = * }"
   let z = set [(1, "I"); (2, "I"); (3, "J")]
-  let actual = Store.stores' 2 d s z
+  let actual = Store.stores' 2 d (Store.tySupp d s) z
   let expected = [
       pstore "r1 : I = { f = r3 }, r2 : I = { f = r3 }, r3 : J = { g = * }"
       pstore "r1 : I = { f = r3 }, r2 : I = { f = r4 }, r3 : J = { g = * }, r4 : J = { g = * }"
       pstore "r1 : I = { f = r3 }, r2 : I = { f = r5 }, r3 : J = { g = * }, r5 : J = { g = * }"
+      pstore "r1 : I = { f = r3 }, r2 : I = { f = null }, r3 : J = { g = * }"
       pstore "r1 : I = { f = r4 }, r2 : I = { f = r3 }, r3 : J = { g = * }, r4 : J = { g = * }"
       pstore "r1 : I = { f = r4 }, r2 : I = { f = r4 }, r3 : J = { g = * }, r4 : J = { g = * }"
       pstore "r1 : I = { f = r4 }, r2 : I = { f = r5 }, r3 : J = { g = * }, r4 : J = { g = * }, r5 : J = { g = * }"
+      pstore "r1 : I = { f = r4 }, r2 : I = { f = null }, r3 : J = { g = * }, r4 : J = { g = * }"
       pstore "r1 : I = { f = r5 }, r2 : I = { f = r3 }, r3 : J = { g = * }, r5 : J = { g = * }"
       pstore "r1 : I = { f = r5 }, r2 : I = { f = r4 }, r3 : J = { g = * }, r4 : J = { g = * }, r5 : J = { g = * }"
       pstore "r1 : I = { f = r5 }, r2 : I = { f = r5 }, r3 : J = { g = * }, r5 : J = { g = * }"
       pstore "r1 : I = { f = r5 }, r2 : I = { f = r6 }, r3 : J = { g = * }, r5 : J = { g = * }, r6 : J = { g = * }"
+      pstore "r1 : I = { f = r5 }, r2 : I = { f = null }, r3 : J = { g = * }, r5 : J = { g = * }"
+      pstore "r1 : I = { f = null }, r2 : I = { f = r3 }, r3 : J = { g = * }"
+      pstore "r1 : I = { f = null }, r2 : I = { f = r4 }, r3 : J = { g = * }, r4 : J = { g = * }"
+      pstore "r1 : I = { f = null }, r2 : I = { f = r5 }, r3 : J = { g = * }, r5 : J = { g = * }"
+      pstore "r1 : I = { f = null }, r2 : I = { f = null }, r3 : J = { g = * }"
     ]
   Assert.AreEqual(set expected, set actual)
