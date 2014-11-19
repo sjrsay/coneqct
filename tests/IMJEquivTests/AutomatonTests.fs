939aeb4d548f03d7304aa4627f2b3699b664e9b5
Steven Ramsay <steven.ramsay@live.com>
2014-11-19 18:02:26 +0000

    Added automaton test.

---
+++ tests/IMJEquivTests/AutomatonTests.fs
@@ -0,0 +1,16 @@
+﻿module IMJEquiv.AutomatonTests
+
+open NUnit.Framework
+
+let fn = System.IO.Path.Combine(__SOURCE_DIRECTORY__,"auto.dot")
+
+[<Test>]
+let ``auto1`` () =
+  let d = pitbl "I = { f: int, m:int -> int }"
+  let g = ptyenv "cxt:void"
+  let t = ptm "new {z:I; m:\x.x}"
+  let c = Canonical.canonise g t
+  let m = Move.ValM Val.VStar
+  let s = pstore ""
+  let a = Automata.fromCanon d g c [m] s
+  System.IO.File.WriteAllText(fn,Automata.toDot a)
