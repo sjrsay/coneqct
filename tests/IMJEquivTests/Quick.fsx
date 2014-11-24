4b87ce3fd3d5e1493550e39e6bf0582156997cb0
Steven Ramsay <steven.ramsay@outlook.com>
2014-11-24 17:49:59 +0000

    Added new automaton tests.

--- tests/IMJEquivTests/Quick.fsx
+++ tests/IMJEquivTests/Quick.fsx
@@ -1,6 +1,6 @@
 ﻿#r @"..\..\packages\NUnit.2.6.3\lib\nunit.framework.dll"
 #r @"..\..\bin\Utils.dll"
-#r @"..\..\bin\IMJEquiv.exe"
+#r @"..\..\IMJEquiv\bin\Debug\IMJEquiv.exe"
 
 open IMJEquiv
 #load "Helpers.fs"
@@ -8,13 +8,11 @@ open IMJEquiv
 
 let fn = System.IO.Path.Combine(__SOURCE_DIRECTORY__,"auto.dot")
 
-let d = pitbl "I = { f: int, m:int -> int }"
-let g = ptyenv ""
-let t = ptm "new {z:I; m:\x.x}"
+let d = pitbl "I = { m:int -> int }, J = { n:int -> int }"
+let g = ptyenv "cxt:J"
+let t = ptm "new {z:I; m:\x.cxt.n(x)}"
 let c = Canonical.canonise g t
-let m = Move.ValM Val.VStar
-let s = pstore ""
+let m = Move.ValM (Val.VReg 1)
+let s = pstore "r1 : I = {}"
 let a = Automata.fromCanon d g c [m] s
 System.IO.File.WriteAllText(fn,Automata.toDot a)
-
-ITbl.methods d "I"
