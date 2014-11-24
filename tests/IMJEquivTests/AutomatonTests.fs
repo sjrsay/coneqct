44df12a46332c2da8fb614c7ed9db54202b177d0
Steven Ramsay <steven.ramsay@live.com>
2014-11-24 17:49:59 +0000

    Added new automaton tests.

--- tests/IMJEquivTests/AutomatonTests.fs
+++ tests/IMJEquivTests/AutomatonTests.fs
@@ -14,3 +14,25 @@ let ``auto1`` () =
   let s = pstore ""
   let a = Automata.fromCanon d g c [m] s
   System.IO.File.WriteAllText(fn,Automata.toDot a)
+
+[<Test>]
+let ``auto2`` () =
+  let d = pitbl "I = { f: int, m:int -> int }"
+  let g = ptyenv "cxt:I"
+  let t = ptm "cxt.m(1)"
+  let c = Canonical.canonise g t
+  let m = Move.ValM (Val.VReg 1)
+  let s = pstore "r1 : I = {}"
+  let a = Automata.fromCanon d g c [m] s
+  System.IO.File.WriteAllText(fn,Automata.toDot a)
+
+[<Test>]
+let ``auto3`` () =
+  let d = pitbl "I = { m:int -> int }, J = { n:int -> int }"
+  let g = ptyenv "cxt:J"
+  let t = ptm "new {z:I; m:\x.cxt.n(x)}"
+  let c = Canonical.canonise g t
+  let m = Move.ValM (Val.VReg 1)
+  let s = pstore "r1 : I = {}"
+  let a = Automata.fromCanon d g c [m] s
+  System.IO.File.WriteAllText(fn,Automata.toDot a)
